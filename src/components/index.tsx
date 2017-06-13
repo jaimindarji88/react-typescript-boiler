1d8acfc71e14e0cbab69a75d80c0057b8e1a2133
Jaimin Darji <jaimin.darji@360pi.com>
2017-06-13 09:51:07 -0400

    commited

---
+++ src/components/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+interface Props{}
+interface State{}
+
+class Routes extends Component<Props, State> {
+    render() {
+        return (
+            <div>
+                Hey
+            </div>
+        );
+    }
+}
+
+export default Routes;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Routes from './components/index'
+
 const App = () => {
   return (
     <div>
